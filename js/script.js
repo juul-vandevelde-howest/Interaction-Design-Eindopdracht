15aeb12bde89c0362b3652d97e7a5bb26da886c0
juul-vandevelde-howest <juul.van.de.velde@student.howest.be>
2023-11-29 16:53:49 +0100

    undid some changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// #region ***  DOM references                           ***********
 const clientId = 'fa36bb52f10c4f928a602c548cd57bdf';
 const clientSecret = '830a8eb690404b52b8fb9dde45765317';
 const authOptions = {
@@ -9,64 +8,7 @@ const authOptions = {
   },
   data: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
 };
-// #endregion
 
-// #region ***  Callback-Visualisation - show___         ***********
-const showGrid = (data, img_urls) => {
-  let html = '';
-  let i = 0;
-  console.info(data);
-  img_urls.forEach((url) => {
-    i++;
-    html += `<button class="grid-artist o-button-reset" data-nr="${i}" data-name="${data[i - 1].track.artists[0].name}" data-id="${data[i - 1].track.artists[0].id}">
-            <img src="${url}" alt="${data[i - 1].track.artists[0].name}">
-        </button>`;
-  });
-  document.querySelector('.js-container').innerHTML = html;
-  listenToClickGrid(data, img_urls);
-};
-// #endregion
-
-// #region ***  Callback-No Visualisation - callback___  ***********
-// #endregion
-
-// #region ***  Data Access - get___                     ***********
-const getGridData = async () => {
-  try {
-    const response = await axios.get('https://api.spotify.com/v1/playlists/37i9dQZF1DXcBWIGoYBM5M', {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('access_token')}`,
-      },
-    });
-
-    const data = response.data;
-    let img_urls = [];
-    for (const track of data.tracks.items) {
-      const data = await getArtist(track.track.artists[0].id);
-      img_urls.push(data.images[0].url);
-    }
-    showGrid(data.tracks.items, img_urls);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-};
-
-const getArtist = async (id) => {
-  try {
-    const response = await axios.get(`https://api.spotify.com/v1/artists/${id}`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('access_token')}`,
-      },
-    });
-    const data = response.data;
-    return data;
-  } catch (error) {
-    console.error('Error:', error);
-  }
-};
-// #endregion
-
-// #region ***  Event Listeners - listenTo___            ***********
 const listenToClickGrid = function (data, img_urls) {
   const body = document.querySelector('body');
   const dialog = document.querySelector('.modal');
@@ -105,9 +47,55 @@ const listenToClickGrid = function (data, img_urls) {
     });
   }
 };
-// #endregion
 
-// #region ***  Init / DOMContentLoaded                  ***********
+const getGridData = async () => {
+  try {
+    const response = await axios.get('https://api.spotify.com/v1/playlists/37i9dQZF1DXcBWIGoYBM5M', {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+      },
+    });
+
+    const data = response.data;
+    let img_urls = [];
+    for (const track of data.tracks.items) {
+      const data = await getArtist(track.track.artists[0].id);
+      img_urls.push(data.images[0].url);
+    }
+    showGrid(data.tracks.items, img_urls);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+const showGrid = (data, img_urls) => {
+  let html = '';
+  let i = 0;
+  console.info(data);
+  img_urls.forEach((url) => {
+    i++;
+    html += `<button class="grid-artist o-button-reset" data-nr="${i}" data-name="${data[i - 1].track.artists[0].name}" data-id="${data[i - 1].track.artists[0].id}">
+            <img src="${url}" alt="${data[i - 1].track.artists[0].name}">
+        </button>`;
+  });
+  document.querySelector('.js-container').innerHTML = html;
+  listenToClickGrid(data, img_urls);
+};
+
+const getArtist = async (id) => {
+  try {
+    const response = await axios.get(`https://api.spotify.com/v1/artists/${id}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+      },
+    });
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
 document.addEventListener('DOMContentLoaded', function () {
   if (!localStorage.getItem('access_token')) {
     axios(authOptions)
@@ -122,4 +110,4 @@ document.addEventListener('DOMContentLoaded', function () {
     getGridData();
   }
 });
-// #endregion
+
